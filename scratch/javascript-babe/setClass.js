e13a076a31c763f9bdb00fa355834bdba355fb18
Sodaba Bashiri <sodaba.bashiri@powercoders.org>
2018-12-14 16:43:29 +0100

    dom event

--- scratch/javascript-babe/setClass.js
+++ scratch/javascript-babe/setClass.js
@@ -4,17 +4,7 @@ function greet(name) {
   console.log('Hello, ' + name);
 }
 
-function setClassTip(event) {
+function setClass(event, classy) {
   let el = document.getElementById('mypara');
-  el.className = 'tip';
-}
-
-function setClassWarning(event) {
-  let el = document.getElementById('mypara');
-  el.className = 'warning';
-}
-
-function clearClass(event) {
-  let el = document.getElementById('mypara');
-  el.className = '';
+  el.className = classy;
 }
