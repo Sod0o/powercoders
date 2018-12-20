bf2431b8e3be99f6f212d3df085166bac1024c8f
Sodaba Bashiri <sodaba.bashiri@powercoders.org>
2018-12-20 13:45:23 +0100

    js

--- scratch/javascript-babe/setClass.js
+++ scratch/javascript-babe/setClass.js
@@ -4,7 +4,24 @@ function greet(name) {
   console.log('Hello, ' + name);
 }
 
-function setClass(event, classy) {
+function setClass(event, className) {
   let el = document.getElementById('mypara');
-  el.className = classy;
+  el.className = className;
 }
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  let makeWarning = document.getElementById('make-warning');
+  makeWarning.addEventListener('click', function(event) {
+   setClass(event, 'warning');
+  });
+
+  let makeTip = document.getElementById('make-tip');
+  makeTip.addEventListener('click', function(event) {
+    setClass(event, 'tip');
+});
+
+  let makeNormal = document.getElementById('make-normal');
+  makeNormal.addEventListener('click', function(event) {
+    setClass(event, '');
+  });
+});
