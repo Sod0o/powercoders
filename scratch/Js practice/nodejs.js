8515a3a3c2ba0aa554b483e098c418f6a465e962
Sodaba Bashiri <sodaba.bashiri@powercoders.org>
2018-12-21 15:29:14 +0100

    js practice

---
+++ scratch/Js practice/nodejs.js
@@ -0,0 +1,53 @@
+console.log('my code is working!');
+
+function addnewpara(text) {
+    let elP = document.createElement('p');
+    let pContent = document.createTextNode(text);  // if i use text in here, i can use it many times with new contents
+    elP.appendChild(pContent);
+    let elBody = document.getElementById('body');
+    elBody.appendChild(elP);
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    document.getElementById('click-me').addEventListener('click', function(event) {
+        addnewpara('Congratulations! You just created a new paragraph. :D');
+    });
+});
+
+function lists() {
+    // DONE: Create the list
+    let elUl = document.createElement('ul');
+
+    // DONE: Create and append first item
+    let li1 = document.createElement('li');
+    let li1Content = document.createTextNode('Hi');
+    li1.appendChild(li1Content);
+    elUl.appendChild(li1);
+
+    // TODO: Create and append second item
+    let li2 = document.createElement('li');
+    let li2Content = document.createTextNode('Hi ');
+    li2.appendChild(li2Content);
+    elUl.appendChild(li2);
+
+    let li3 = document.createElement('em');
+    let li3Content = document.createTextNode('Hoi');
+    li3.appendChild(li3Content);
+    li2.appendChild(li3);
+
+    // TODO: Create and append third item
+    let li4 = document.createElement('li');
+    let li4Content = document.createTextNode('Hi Hoi Hello');
+    li4.appendChild(li4Content);
+    elUl.appendChild(li4);
+
+    // DONE: Append the list to the body
+    let elBody = document.getElementById('body');
+    elBody.appendChild(elUl);
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+    document.getElementById('click-me', function (event) {
+        lists();
+    });
+});
