294f38c98dcb1c7a2233e617b77ba4a5ca8f31b5
Sodaba Bashiri <sodaba.bashiri@powercoders.org>
2019-03-09 18:23:46 +0100

    changes in margin of css file that has been renamed to style.less and some changes to html file and Gruntfile.js - margin left and right changed to @default-left/right-margin in style.css - href="../css/style.css" changed to href="css/style.min.css in index.prod.html's link element - src="../static/soda.png changed to src="../static/soda.png in index.prod.html's image element

--- shopping-mvc/Gruntfile.js
+++ shopping-mvc/Gruntfile.js
@@ -27,13 +27,52 @@ module.exports = function (grunt) {
         }]
       }
     },
-    // ...
+    cssmin: {
+      target: {
+        files: {
+          'build/css/style.min.css': ['src/css/*.css']
+        }
+      }
+    },
+    less: {
+      build: {
+        files: {
+          'src/css/style.css': 'src/css/style.less'
+        }
+      }
+    },
+    watch: {
+      less: {
+        files: 'src/css/*.less',
+        tasks: ['less']
+      },
+      css: {
+        files: 'src/css/*.css,',
+        tasks: ['cssmin']
+      },
+      scripts: {
+        files: 'src/js/*.js',
+        tasks: ['uglify']
+      },
+      html: {
+        files: 'src/html/index.prod.html',
+        tasks: ['copy:html']
+      },
+      static: {
+        files: 'src/static/*.*',
+        tasks: ['copy:static']
+      }
+    },
   });
+
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify-es');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less')
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify', 'copy']);
+  grunt.registerTask('default', ['less', 'cssmin', 'uglify', 'copy']);
 };
